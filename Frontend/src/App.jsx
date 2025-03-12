950477458c103361685887d3557073ca96386a74
siddharth rukadikar <siddharthrukadikar@gmail.com>
2025-03-12 20:34:23 +0530

    feat: implement routing with react-router-dom and add user/captain login/signup pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,22 @@
-
+import { Route, Routes } from "react-router-dom"
+import Home from "./Pages/Home"
+import UserLogin from "./Pages/UserLogin"
+import UserSignup from "./Pages/UserSignup"
+import CaptainLogin from "./Pages/CaptainLogin"
+import CaptainSignup from "./Pages/CaptainSignup"
 
 
 function App() {
 
   return (
     <>
-      <h1 className='text-2xl '>app</h1>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/signup" element={<UserSignup/>}/>
+        <Route path="/login" element={<UserLogin/>}/>
+        <Route path="/captain-signup" element={<CaptainSignup/>}/>
+        <Route path="/captain-login" element={<CaptainLogin/>}/>
+      </Routes>
     </>
   )
 }
---
+++ Frontend/src/Pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        
+        <div>
+
+        </div>
+        <div>
+            <img src="../assets/Screenshot (68).png" alt="home picture" />
+        </div>
+    </div>
+  )
+}
+
+export default Home
