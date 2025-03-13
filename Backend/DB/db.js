b927d98b41d9ce05c2d1865641801661006a7da5
siddharth rukadikar <siddharthrukadikar@gmail.com>
2025-03-13 17:47:46 +0530

    fix: correct typo in MongoDB connection log message

--- Backend/DB/db.js
+++ Backend/DB/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 async function connectToDB(){
     try {
        const instance= await mongoose.connect(process.env.MONGODB_URI)
-       console.log(`MangoDB connected !! DB Host: ${instance.connection.host}`);
+       console.log(`MongoDB connected !! DB Host: ${instance.connection.host}`);
        
     } catch (error) {
         console.log("Mongodb connection Error:",error);
